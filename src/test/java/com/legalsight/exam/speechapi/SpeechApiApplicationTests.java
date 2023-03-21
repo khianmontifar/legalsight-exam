84587aa31c15069b76ef62db05b3a56531beac36
khianmontifar <khian.montifar@gmail.com>
2023-03-21 12:19:50 +0800

    Initial commit

---
+++ src/test/java/com/legalsight/exam/speechapi/SpeechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legalsight.exam.speechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
